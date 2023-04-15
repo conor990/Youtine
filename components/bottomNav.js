419f88a3c497d85720eb499e3013dd666c3ad90e
Conor Odoemene <conor99055@gmail.com>
2023-04-15 17:21:21 +0100

    debugging

--- components/bottomNav.js
+++ components/bottomNav.js
@@ -4,128 +4,144 @@ import HomeScreen from '../screens/HomeScreen';
 import MindScreen from '../screens/MindScreen';
 import BodyScreen from '../screens/BodyScreen';
 import MealsScreen from '../screens/MealsScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+
 
 const Tab = createBottomTabNavigator();
 
 
-const BottomNav = () => {
+const BottomNav = ({ navigation }) => {
 
-    return(
-        <Tab.Navigator
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          position: 'absolute',
+          bottom: 25,
+          left: 20,
+          right: 20,
+          backgroundColor: '#fff',
+          borderRadius: 35,
+          height: 55,
+          width: 320,
+          paddingBottom: 10,
+          marginLeft: 25,
+          ...styles.shadow
+        }
 
-            screenOptions={{
-                headerShown:false,
-                tabBarShowLabel:false,
+      }}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} options={{
+        tabBarIcon: ({ focused }) => (
+          <View>
+            <Image
+              source={require('../components/assets/bottomNavBar/homeBlack.png')}
+              resizeMode='contain'
+              style={{
+                width: 30,
+                height: 30,
+                shadowColor: focused ? 'green' : 'grey',
 
-                tabBarStyle:{
-                    position:'absolute',
-                    bottom:25,
-                    left: 20,
-                    right: 20,
-                    backgroundColor: '#fff',
-                    borderRadius: 35,
-                    height: 55,
-                    width:320,
-                    paddingBottom:10,
-                    marginLeft:25,
-                    ...styles.shadow 
-                }
+                shadowOffset: {
+                  width: 1,
+                  height: 1
+                },
+                shadowOpacity: 1,
+              }}
 
-            }}
-        >
-            <Tab.Screen name="Home" component={HomeScreen} options = {{
-                tabBarIcon: ({focused}) => (
-                    <View>
-                        <Image
-                            source={require('../components/assets/bottomNavBar/homeBlack.png')}
-                            resizeMode='contain'
-                            style={{        
-                                width: 30,
-                                height: 30,
-                                shadowColor: focused ? 'green' : 'grey',
-                            
-                                shadowOffset: {
+
+            />
+          </View>
+        ),
+      }} />
+
+      <Tab.Screen name="Mind" component={MindScreen} options={{
+        tabBarIcon: ({ focused }) => (
+          <View>
+            <Image
+              source={require('../components/assets/bottomNavBar/brainBlack.png')}
+              resizeMode='contain'
+              style={{
+                width: 30,
+                height: 30,
+                shadowColor: focused ? 'green' : 'grey',
+
+                shadowOffset: {
+                  width: 1,
+                  height: 1
+                },
+                shadowOpacity: 1,
+              }}
+            />
+          </View>
+        ),
+      }} />
+
+      <Tab.Screen name="Profile" component={ProfileScreen} options={{
+        tabBarIcon: ({ focused }) => (
+          <View>
+            <Image
+              source={require('../components/assets/bottomNavBar/weightBlack.png')}
+              resizeMode='contain'
+              style={{
+                width: 30,
+                height: 30,
+                shadowColor: focused ? 'green' : 'grey',
+
+                shadowOffset: {
+                  width: 1,
+                  height: 1
+                },
+                shadowOpacity: 1,
+              }}
+            />
+          </View>
+        ),
+      }} />
+
+      <Tab.Screen name="Body" component={BodyScreen} options={{
+        tabBarIcon: ({ focused }) => (
+          <View>
+            <Image
+              source={require('../components/assets/bottomNavBar/weightBlack.png')}
+              resizeMode='contain'
+              style={{
+                width: 30,
+                height: 30,
+                shadowColor: focused ? 'green' : 'grey',
+
+                shadowOffset: {
+                  width: 1,
+                  height: 1
+                },
+                shadowOpacity: 1,
+              }}
+            />
+          </View>
+        ),
+      }} />
+
+      <Tab.Screen name="Meals" component={MealsScreen} options={{
+        tabBarIcon: ({ focused }) => (
+          <View>
+            <Image
+              source={require('../components/assets/bottomNavBar/mealBlack.png')}
+              resizeMode='contain'
+              style={{
+                width: 30,
+                height: 30,
+                shadowColor: focused ? 'green' : 'grey',
+
+                shadowOffset: {
                                   width:1,
                                   height: 1
                                 },
                                 shadowOpacity:1,
                             }}
-                            
-                            
                         />
-                    </View>
-                ),
-            }} />
-
-
-
-            <Tab.Screen name="Mind" component={MindScreen} options = {{
-                tabBarIcon: ({focused}) => (
-                    <View>
-                        <Image
-                            source={require('../components/assets/bottomNavBar/brainBlack.png')}
-                            resizeMode='contain'
-                            style={{        
-                                width: 30,
-                                height: 30,
-                                shadowColor: focused ? 'green' : 'grey',
-                            
-                                shadowOffset: {
-                                  width:1,
-                                  height: 1
-                                },
-                                shadowOpacity:1,
-                            }}
-                        />
-                    </View>
-                ),
-            }} />
-
-
-
-
-            <Tab.Screen name="Body" component={BodyScreen} options = {{
-                tabBarIcon: ({focused}) => (
-                    <View>
-                        <Image
-                            source={require('../components/assets/bottomNavBar/weightBlack.png')}
-                            resizeMode='contain'
-                            style={{        
-                                width: 30,
-                                height: 30,
-                                shadowColor: focused ? 'green' : 'grey',
-                            
-                                shadowOffset: {
-                                  width:1,
-                                  height: 1
-                                },
-                                shadowOpacity:1,
-                            }}
-                        />
-                    </View>
-                ),
-            }} />
-
-
-
-            <Tab.Screen name="Meals" component={MealsScreen} options = {{
-                tabBarIcon: ({focused}) => (
-                    <View>
-                        <Image
-                            source={require('../components/assets/bottomNavBar/mealBlack.png')}
-                            resizeMode='contain'
-                            style={{        
-                                width: 30,
-                                height: 30,
-                                shadowColor: focused ? 'green' : 'grey',
-                            
-                                shadowOffset: {
-                                  width:1,
-                                  height: 1
-                                },
-                                shadowOpacity:1,
-                            }}
-                        />
+                        
                     </View>
                 ),
             }} />
--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,29 +1,16 @@
-import BottomNavComponent from '../components/BottomNav';
-import { StyleSheet, Text, View, SafeAreaView, Image, Button } from 'react-native';
-import Tabs from "../navigation/tabs";
 import BottomNav from "../components/BottomNav";
+import { StyleSheet, View, Text} from 'react-native';
 
 export default function ProfileScreen({ navigation }){
     return(
         <View style={styles.container}>
-            <Image style={styles.logo} source={require('../components/assets/logoLong.png')} />
-  
             <View style={styles.mainStage}>
-  
-
-            <BottomNav />
-        
+                
+                <BottomNav navigation={navigation} />
+            </View>
         </View>
-  
-      </View>
     );
 }
-
-
-
-
-
-
 
 const styles = StyleSheet.create({
   container: {
@@ -31,7 +18,6 @@ const styles = StyleSheet.create({
     width:'100%',
     backgroundColor: '#6e9f7b',
   },
-
   mainStage: {
     backgroundColor: '#cfd1d0',
     flex:1,
@@ -41,16 +27,4 @@ const styles = StyleSheet.create({
     justifyContent:'space-evenly',
     flexWrap: 'wrap'
   },
-
-
-  logo: {
-    width:155,
-    height:30,
-    margin:20,
-    marginTop:50
-  },
-
-
 });
-
-
